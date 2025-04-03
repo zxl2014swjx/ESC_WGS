6f545ef79b880c6d61a9edc391dfccebd7be0616
zxl2014swjx <54253876+zxl2014swjx@users.noreply.github.com>
2025-04-03 16:29:07 +0800

    Create run.sh

---
+++ call CNV/ichorCNA/run.sh
@@ -0,0 +1,5 @@
+for i in E1 E10 E15 E18 E2 E23 E24 E26 E31 E33 E35 E47 E51;do echo "sh cal_ichorCNA.sh /mnt/X500/farmers/zhuxl/Project/ESC_Cheng_WGS/13_normal_bam/$i\TD/recaled.bam /mnt/X500/farmers/zhuxl/Project/ESC_Cheng_WGS/13_normal_bam/$i\ND/recaled.bam $i\TD $i\ND $i\TD_$i\ND" >>run.sh ;done &
+
+
+
+
